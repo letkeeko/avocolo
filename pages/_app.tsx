5759f2e82394ba1debbd6c08d5d75585a8782eb0
Keeko <letkeeko@gmail.com>
2022-05-24 21:25:36 +1000

    add loading progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../css/globals.css";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
+import { COLOR } from "../components/variables";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress color={COLOR.green} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
